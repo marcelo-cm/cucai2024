63c707de6629080d8ddcb5937fdbaf9c36a77e47
Marcelo ChamanMallqui <marcechaman@gmail.com>
2024-01-27 19:48:32 -0500

    edii grant route

--- app/ediigrant/page.tsx
+++ app/ediigrant/page.tsx
@@ -3,14 +3,14 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
-function QMINDTicketPage() {
+function EDIIGrant() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push("https://forms.gle/s5GJwx1oYhxTSQJG7");
+    router.push("https://tally.so/r/nrP6dL");
   }, [router]);
 
   return <div className="text-white">see u soon. redirecting...</div>;
 }
 
-export default QMINDTicketPage;
+export default EDIIGrant;
