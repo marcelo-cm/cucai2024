0c99db0805f54979a8f491659985bd6ecb91b224
gonzaloandresoto <gonzandresoto@gmail.com>
2024-02-16 21:51:32 -0500

    improved redirect if user signed in and onboarded to prevent infinite loop

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,14 @@
 import OnboardingCard from '@/components/onboarding/OnboardingCard';
-import { checkOnboarded } from '@/lib/actions/user.actions';
+import { checkOnboarded, getUserId } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
 
 export default async function Onboarding() {
-  await checkOnboarded();
+  // REDIRECT TO DASHBOARD IF THE USER IS LOGGED IN AND HAS ALREADY ONBOARDED
+  const userId = await getUserId();
+  const onboarded = await checkOnboarded(userId);
+  if (onboarded) {
+    redirect('/dashboard');
+  }
   return (
     <div className='flex grow justify-center p-4'>
       <OnboardingCard />
