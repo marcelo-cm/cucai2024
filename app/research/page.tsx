e3e33990e0c1a362a991ef7267b676c4aec301b1
gonzaloandresoto <gonzandresoto@gmail.com>
2024-02-20 22:20:40 -0500

    server fetch & set page number

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
-import page from '../page';
+import Hero from '@/components/research/Hero';
+import ResearchPapers from '@/components/research/ResearchPapers';
+import getResearchPapers from '@/lib/actions/general.actions';
 
-const Research = () => {
-  return <div>page</div>;
-};
+const paginationLimit = 2;
 
-export default Research;
+export default async function page({
+  searchParams: { page = '1' },
+}: {
+  searchParams: { page: string };
+}) {
+  let pageNumber;
+
+  try {
+    pageNumber = parseInt(page) || 1;
+    if (pageNumber < 1) pageNumber = 1;
+  } catch (error) {
+    pageNumber = 1;
+  }
+
+  const response = await getResearchPapers(pageNumber, paginationLimit);
+  const papers = response?.papers;
+  const total = response?.total || 0;
+  const canNext = total > pageNumber * paginationLimit || false;
+  const canPrev = pageNumber > 1 || false;
+
+  return (
+    <div className='flex grow flex-col'>
+      <Hero />
+      <ResearchPapers
+        papers={papers}
+        pageNumber={pageNumber}
+        total={total}
+        canNext={canNext}
+        canPrev={canPrev}
+        paginationLimit={paginationLimit}
+      />
+    </div>
+  );
+}
